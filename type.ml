7d2e9b7fd97e2e501c1163934954ceb191705376
udondaisuki <mythsuma@yahoo.co.jp>
2013-04-15 00:34:17 +0900

    コメントの追加とリファクタリング

--- type.ml
+++ type.ml
@@ -1,17 +1,13 @@
+(* 型環境 *)
 module Env = Map.Make (String)
 
 type t =
   | Int
   | Bool
+  (* 型変数 *)
   | Var of t option ref
   | Fun of t * t
   | List of t
-
-let rec flatten = function
-  | Var { contents = Some (t) } -> flatten t
-  | Fun (t1, t2) -> Fun (flatten t1, flatten t2)
-  | List (t) -> List (flatten t)
-  | t -> t
 
 let to_string t =
   let counter = ref 1 in
@@ -29,34 +25,37 @@ let to_string t =
             label
         end
     | Var { contents = Some (t) } -> to_string_aux t
-    | Fun ((Fun _) as t1, t2) -> "(" ^ to_string_aux t1 ^ ") -> " ^ to_string_aux t2
+    | Fun (Fun _ as t1, t2) -> "(" ^ to_string_aux t1 ^ ") -> " ^ to_string_aux t2
     | Fun (t1, t2) -> to_string_aux t1 ^ " -> " ^ to_string_aux t2
-    | List ((Fun _) as t) -> "(" ^ to_string_aux t ^ ") list"
+    | List (Fun _ as t) -> "(" ^ to_string_aux t ^ ") list"
     | List (t) -> to_string_aux t ^ " list" in
   counter := 1;
   dic := [];
   to_string_aux t
 
-let rec circulation var = function
+(* 方程式に解が存在するか確かめる *)
+let rec occur var = function
   | Var (var') when var == var' -> true
   | Int | Bool | Var { contents = None } -> false
-  | Fun (t1, t2) -> circulation var t1 || circulation var t2
-  | Var { contents = Some (t) } | List (t) -> circulation var t
+  | Fun (t1, t2) -> occur var t1 || occur var t2
+  | Var { contents = Some (t) } | List (t) -> occur var t
 
+(* 型を単一化する *)
 let rec unify t1 t2 =
   match t1, t2 with
   | Int, Int | Bool, Bool -> ()
-  | (Var ({ contents = None } as var) as t1), t2 | t2, (Var ({ contents = None } as var) as t1) ->
-      if t1 <> t2 then
-        if circulation var t2 then raise (Failure "recursive type")
-        else var := Some (t2)
-  | Var { contents = Some (t1) }, t2 | t2, Var { contents = Some (t1) } ->
+  | Var (var1), Var (var2) when var1 == var2 -> ()
+  | Var ({ contents = None } as var), t2 | t2, Var ({ contents = None } as var) ->
+      if occur var t2 then raise (Failure "recursive type")
+      else var := Some (t2)
+  | Var { contents = Some (t1) }, t2 | t2, Var { contents = Some (t1) }
+  | List (t1), List (t2) ->
       unify t1 t2
   | Fun (t11, t12), Fun (t21, t22) ->
       unify t11 t21;
       unify t12 t22
-  | List (t1), List (t2) -> unify t1 t2
 
+(* 与えられた型環境で式に型を付ける *)
 let rec typing env = function
   | Exp.Int _ -> Int
   | Exp.Bool _ -> Bool
@@ -75,12 +74,10 @@ let rec typing env = function
       unify t2 (typing env e3);
       t2
   | Exp.Let (x, e1, e2) -> 
-      let t1 = typing env e1 in
-      typing (Env.add x t1 env) e2
+      typing (Env.add x (typing env e1) env) e2
   | Exp.Fun (x, e) ->
       let alpha = Var (ref None) in 
-      let t = typing (Env.add x alpha env) e in
-      Fun (alpha, t)
+      Fun (alpha, typing (Env.add x alpha env) e)
   | Exp.App (e1, e2) ->
       let alpha = Var (ref None) in 
       unify (typing env e1) (Fun (typing env e2, alpha));
@@ -104,5 +101,14 @@ let rec typing env = function
       unify t2 (typing (Env.add y t1 (Env.add x alpha env)) e3);
       t2
 
+(* この型の表現ではある型tに対してtとVar { contents = Some (t) }の
+   二通りができて気持ち悪かったので、前者に統一する関数を定義している *)
+let rec flatten = function
+  | Var { contents = Some (t) } -> flatten t
+  | Fun (t1, t2) -> Fun (flatten t1, flatten t2)
+  | List (t) -> List (flatten t)
+  | t -> t
+
+(* 関数の合成 *)
 let ( >> ) f g x = g (f x)
 let of_Exp = typing Env.empty >> flatten
--- type.mli
+++ type.mli
@@ -1,3 +1,6 @@
+(* 型を表す型 *)
 type t
+(* 式に型を付ける *)
 val of_Exp : Exp.t -> t
+(* 型を文字列で表現する *)
 val to_string : t -> string
